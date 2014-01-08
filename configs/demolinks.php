725d18f04905238ffffd10d27c9b299e6e75cbb6
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-08 17:35:23 +0100

    GNR-9 modified demo link configuration

--- configs/demolinks.php
+++ configs/demolinks.php
@@ -1,2 +1,4 @@
 <a href="#page/auth" class="btn btn-default">Login Demo</a>
 <a href="#page/auth/modal" class="btn btn-default">Login-Modal Demo</a>
+<a href="#page/auth/demo-register" class="btn btn-default">Register-Process</a>
+<a href="#page/auth/demo-register-modal" class="btn btn-default">Register-Process-Modal</a>
