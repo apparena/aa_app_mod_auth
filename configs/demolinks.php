9f796e148b0504b5f69f204c3b0c828b3e14364a
Marcus Merchel <kontakt@marcusmerchel.de>
2014-01-09 18:15:31 +0100

    GNR-9 added demolink break

--- configs/demolinks.php
+++ configs/demolinks.php
@@ -1,3 +1,5 @@
+</div>
+<div class="btn-group btn-group-justified">
 <a href="#page/auth" class="btn btn-default">Login Demo</a>
 <a href="#page/auth/modal" class="btn btn-default">Login-Modal Demo</a>
 <a href="#page/auth/demo-register" class="btn btn-default">Register-Process</a>
